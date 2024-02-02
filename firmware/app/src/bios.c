1f92fcd0f502765287d9c865b8486f879069e6b1
Robert Szczepanski <rszczepanski@antmicro.com>
2024-02-02 17:42:18 +0100

    Add initial Zephyr app structure
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

--- firmware/app/src/bios.c
+++ firmware/app/src/bios.c
@@ -1,7 +1,14 @@
 /*
- * Copyright 2023 Antmicro
+ * Copyright 2024 Antmicro
  * SPDX-License-Identifier: Apache-2.0
  */
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
+#include <app_version.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 #include "uart_stdio.h"
 #include "csr.h"
