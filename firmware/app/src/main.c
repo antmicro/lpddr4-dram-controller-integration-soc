d2cf6238dafddf9057b9a91f4b6ac6d97924e919
Mateusz Sierszulski <msierszulski@antmicro.com>
2024-02-08 15:38:08 +0100

    [#54681] run Zephyr shell as an example
    Signed-off-by: Mateusz Sierszulski <msierszulski@antmicro.com>

---
+++ firmware/app/src/main.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2024 Antmicro
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/shell/shell.h>
+#include <version.h>
+#include <zephyr/logging/log.h>
+#include <stdlib.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
+#include <ctype.h>
+
+// #include "csr.h"
+
+
+int main(void) {
+	// printf("Hello world! %s\n", CONFIG_BOARD);
+
+	return 0;
+}
