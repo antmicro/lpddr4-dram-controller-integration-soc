db4f6b79b8551b38689e9bcb59a7795e550b67df
Mateusz Sierszulski <msierszulski@antmicro.com>
2024-02-14 14:33:12 +0100

    [#54681] Working base shell sample
    Signed-off-by: Mateusz Sierszulski <msierszulski@antmicro.com>

--- firmware/app/src/main.c
+++ firmware/app/src/main.c
@@ -5,22 +5,8 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/shell/shell.h>
-#include <version.h>
-#include <zephyr/logging/log.h>
-#include <stdlib.h>
-#include <zephyr/drivers/uart.h>
-#include <zephyr/usb/usb_device.h>
-#include <ctype.h>
 
 int main(void)
 {
-	printf("Hello world! %s\n", CONFIG_BOARD);
-
-	while (1)
-	{
-		printf("Hello world! %s\n", CONFIG_BOARD);
-		k_msleep(1);
-	}
-
 	return 0;
 }
